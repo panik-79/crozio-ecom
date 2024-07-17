7211a2b7e0ffec71b70abe8c07d008629f8818d0
panik-79 <pujanparekh2609@gmail.com>
2024-07-17 10:24:09 +0530

    bug fixed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,32 +1,31 @@
-import express from 'express';
+import express from "express";
 import {
-    createUser,
-    loginUser,
-    updateUserProfile,
-    deleteUser,
-    getUserProfile,
-    logoutUser,
-    getAllUsers,
-    getUserProfileById,
-    updateUserById
-} from '../controllers/userController.js';
-import { authenticate, checkAdmin } from '../middlewares/authMiddleware.js';
+	createUser,
+	loginUser,
+	updateUserProfile,
+	deleteUser,
+	getUserProfile,
+	logoutUser,
+	getAllUsers,
+	getUserProfileById,
+	updateUserById,
+} from "../controllers/userController.js";
+import { authenticate, checkAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/auth', loginUser);
-router.post('/register', createUser);
-router.post('/logout', authenticate, logoutUser);
-router.get('/profile', authenticate, getUserProfile);
-router.put('/profile', authenticate, updateUserProfile);
+router.post("/auth", loginUser);
+router.post("/register", createUser);
+router.post("/logout", authenticate, logoutUser);
+router.get("/profile", authenticate, getUserProfile);
+router.put("/profile", authenticate, updateUserProfile);
 
-router.route('/:id')
-    .delete(authenticate, checkAdmin, deleteUser)
-    .get(authenticate, checkAdmin, getUserProfileById)
-    .put(authenticate, checkAdmin, updateUserById)
+router
+	.route("/:id")
+	.delete(authenticate, checkAdmin, deleteUser)
+	.get(authenticate, checkAdmin, getUserProfileById)
+	.put(authenticate, checkAdmin, updateUserById);
 
-router.route('/').
-post(createUser).
-get(authenticate, checkAdmin, getAllUsers);
+router.route("/").post(createUser).get(authenticate, checkAdmin, getAllUsers);
 
 export default router;
